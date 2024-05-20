67bae83ad12e5a163cacaf2affcb9bc9e714aa58
UR-xiaoyang <2422286329@qq.com>
2024-05-21 00:27:17 +0800

    将pandas更换为openpyxl

--- README.md
+++ README.md
@@ -3,19 +3,15 @@
 ## 说明
 使用前需要去往[智谱AI开放平台](https://open.bigmodel.cn/)去申请一枚API Key,和去[百度云](https://cloud.baidu.com/)申请OCR（不带位置）的两个key来接入OCR
 
-所用技术栈：tkinter,pandas,zhipuai,urllib
+所用技术栈：tkinter,openpyxl,zhipuai,urllib
 所使用的大模型：百度OCR标准识别，ChatGLM-4
 
-## V1.1更新日志
-* 使用API对大模型进行调用使得速度得到少许的提升
-* 使用了更加先进的ChatGLM4模型输出得更加的稳定
-* 废弃了V1.0版本
-* 修复了无法更换招聘岗位的问题
-* 发布第一个发布版V1.1
-* 导出了requirements.txt
+## V1.2更新日志
+* 将pandas替换为openpyxl
 
 ## 启动项目
 ```bash
+pip install -r requirements.txt
 python 项目！启动！.py
 ```
 然后按照提示依次输入所需要的参数即可
--- 导出.py
+++ 导出.py
@@ -1,7 +1,7 @@
 # 导出为Excel表
 # 表头为：姓名，年龄，学位，毕业学校，工龄，项目经历，总体分析，AI建议
-import pandas as pd
 
+from openpyxl import Workbook
 def 输出Excel(工程文件夹,data):
     # 检查
     longest_key = max(data, key=lambda x: len(data[x]) if isinstance(data[x], list) else 1)
@@ -10,11 +10,21 @@ def 输出Excel(工程文件夹,data):
         if len(data[i]) < len(data[longest_key]):
             for j in range(len(data[longest_key]) - len(data[i])):
                 data[i].append("Null")
-    # 将字典转换为DataFrame
-    df = pd.DataFrame(data)
+    # 将字典导出为Excel表
+    # 创建一个新的工作表
+    工作表 = Workbook()
+    # 设为活动
+    活动的工作表 = 工作表.active
+    # 写入表头
+    for 列号, 列名 in enumerate(data.keys(), start=1):
+        活动的工作表.cell(row=1, column=列号, value=列名)
+    # 写入数据
+    for 行号 in range(2, len(data[longest_key]) + 2):
+        for 索引,键名 in enumerate(data.keys()):
+            活动的工作表.cell(row=行号, column=索引 + 1, value=data[键名][行号 - 2])
 
-    # 导出到Excel文件，如果你使用的是Excel 2010或更新的版本，文件扩展名应为.xlsx
-    filename = f"{工程文件夹}/简历汇总表.xlsx"
-    df.to_excel(filename, index=False, engine='openpyxl')
 
+
+    filename = f"{工程文件夹}/简历分析.xlsx"
+    工作表.save(filename)
     print(f"数据已导出到Excel文件：{filename}")
